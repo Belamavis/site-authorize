0c654733b5d32fbd462d407119e2fa0d410ecfab
Dima <BashkovDA@yandex.ru>
2016-10-25 16:06:39 +0300

    Добавлен код HTMLЭ

--- join.php
+++ join.php
@@ -1,9 +1,4 @@
 <?php 
-
-# 
-#
-#
-
 	include_once("config.php");
 	checkLoggedIn("no");
 	
@@ -31,5 +26,28 @@
 			}
 		}
 ?>
+
+<html>
+	<head>
+		<title><?php print $title; ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=uft-8">
+	</head>
+	<body>
+		<h1><?php print $title; ?></h1>
+		<?php
+			if(!empty($messages)){
+				displayErrors($messages);
+			}
+		?>
+		<form action="<?php print $_SERVER["PHP_SELF"]; ?>" method="POST">
+			<table>
+				<tr><td>Логин:</td><td><input type="text" name="login" value="<?php print isset($_POST["login"]) ? $_POST["login"] : "" ; ?>" maxlength="15"></td></tr>
+				<tr><td>Пароль:</td><td><input type="password" name="password" value="" maxlength="15"></td></tr>
+				<tr><td>Повторить пароль:</td><td><input type="password" name="password2" value="" maxlength="15"></td></tr>
+				<tr><td>&nbsp;</td><td><input name="submit" type="submit" value="Submit"></td></tr>
+			</table>
+		</form>
+	</body>
+</html>
 
 
